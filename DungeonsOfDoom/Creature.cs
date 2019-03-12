7b522589fe527428366ff63b4ace3e1f0de00b12
Teknikos <teknikos@hotmail.com>
2019-03-12 15:49:49 +0100

    Added a Creature.cs class

---
+++ DungeonsOfDoom/Creature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsOfDoom
+{
+    abstract class Creature
+    {
+        public int Health { get; set; }
+        public string Name { get; set; }
+
+        public Creature(int baseHealth)
+        {
+            Health = baseHealth;
+        }
+    }
+}
